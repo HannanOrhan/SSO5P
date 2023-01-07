e69f3742ab4b788aae675c7475e85fac179df70b
Hannan ORHAN <hannanorhn00@gmail.com>
2023-01-07 11:31:33 -0800

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -3,5 +3,7 @@ package first_package;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Class");
+        //some changes
+         System.out.println("First Class");
     }
 }
