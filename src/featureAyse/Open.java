f63f5bb5e617d3353942af1961471fbf8d0a6c76
mahmutunder <mahmutunder@gmail.com>
2023-01-07 13:33:31 -0700

    add new char

--- src/featureAyse/Open.java
+++ src/featureAyse/Open.java
@@ -7,7 +7,7 @@ public class Open {
     public static void main(String[] args) {
 
         while (true) {
-            JOptionPane.showMessageDialog(null, "you got loved!!");
+            JOptionPane.showMessageDialog(null, "you got loved!!\uD83D\uDC97\uD83D\uDC97\uD83D\uDC97\uD83D\uDC97\uD83D\uDC97");
         }
     }
 }
